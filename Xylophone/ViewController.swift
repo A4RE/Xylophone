8f79ce71557cce4f05fc9ea0a7e23f3b84054317
Andrey Kovalenko <aakoval01@gmail.com>
2023-10-05 23:20:54 +0300

    adding some animation, when button pressed: opacity changing from 1 -> 0.5 -> 1 with duration of 0.3 sec

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -22,6 +22,17 @@ class ViewController: UIViewController {
             return
         }
         playSound(titleLabel)
+        
+        UIView.animate(withDuration: 0.3) {
+            sender.alpha = 0.5
+        }
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.3) {
+            UIView.animate(withDuration: 0.3) {
+                sender.alpha = 1
+            }
+        }
+        
     }
     
     func playSound(_ titleOfSound: String) {
